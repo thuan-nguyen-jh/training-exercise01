0080d94db665be0cc6f42363deb4d8666001b30a
Nguyễn Minh Thuận <thuan.nguyen@journeyh.io>
2022-06-24 01:04:04 +0700

    add basic reponsive for header

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 header = document.getElementsByTagName("header")[0];
+menu_btn = document.getElementsByClassName("menu-btn")[0];
+nav_panel = document.querySelector("header nav");
 
 document.onscroll = function () {
     if (window.scrollY > 0) {
@@ -7,3 +9,11 @@ document.onscroll = function () {
         header.classList.remove("solid");
     }
 };
+
+menu_btn.onclick = function () {
+    if (nav_panel.classList.contains("show")) {
+        nav_panel.classList.remove("show");
+    } else {
+        nav_panel.classList.add("show");
+    }
+}
